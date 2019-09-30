d4cf6369e5db4b804d9015105c9d6a7b56197dd3
AndreaVasquezHerrera <and.vasquez.herrera@gmail.com>
2019-09-30 10:04:09 -0300

    actualiza filtro

--- src/data.js
+++ src/data.js
@@ -1,10 +1,22 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
-};
+window.dataManager = {
+   
+  filterRick : (data) => {
+      let items = data.filter(element => element.name.indexOf('Rick') > -1);
+      console.log(items)
+      return items;
+  },
+  filterMorty :(data) => {
+      let items = data.filter(element => element.name.indexOf('Morty') > -1);
+      return items;
+  },
+  
+  filterOtrosPersonajes : (data) => {
+  let items = data.filter(element => element.name.includes('Rick') == false && element.name.includes('Morty') == false  );
+  console.log(items) 
+  return items;
+  
+ },
+  
+}  
 
 window.example = example;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,83 @@
 /* Manejo del DOM */
+const data = RICKANDMORTY.results;
+const card = document.getElementById("rick-mory-card");
+const buttonRicks = document.getElementById("button-ricks");
+const buttonMorty = document.getElementById("button-morty");
+const buttonOtrosPersonajes = document.getElementById("button-otrosPersonajes");
+
+
+//Muestra lista de todos los personajes en tarjetas
+let dataSee = (data) => {
+    let str = ''
+    data.forEach(element => {
+        console.log(element)
+        str +=`<div class="card"><p> Nombre: ${element.name}<p>
+        <p>Estado: ${element.status}<p>
+        <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src ="${element.image}" id="imagen"> </div>`
+
+
+    });
+    card.innerHTML = str
+}
+dataSee(data)
+
+//Para Filtrar versiones de Rick
+let resultRick = (data) => {
+    let result = window.dataManager.filterRick(data);
+    let str = 'Rick'
+    result.forEach(element => {
+        str += `<div class="card"><p> Nombre: ${element.name}<p>
+     <p>Estado: ${element.status}<p>
+     <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src = "${element.image}"width="80%" height="90%"> </div>`
+    });
+     
+    card.innerHTML = str;
+    
+}
+
+
+buttonRicks.addEventListener('click', () => {
+    resultRick(data)
+});
+
+//Para Filtrar versiones de Morty
+let resultMorty = (data) => {
+    let result = window.dataManager.filterMorty(data);
+    let str = 'Morty'
+    result.forEach(element => {
+    str += `<div class="card"><p> Nombre: ${element.name}<p>
+     <p>Estado: ${element.status}<p>
+     <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src = "${element.image}"width="80%" height="90%"> </div>`
+    });
+     
+    card.innerHTML = str;
+    
+}
+
+
+buttonMorty.addEventListener('click', () => {
+    resultMorty(data)
+});
+
+//Para Filtrar todos los demas personajes 
+let resultOtrosPersonajes = (data) => {
+    let result = window.dataManager.filterOtrosPersonajes(data);
+    let str = ''
+    result.forEach(element => {
+     str += `<div class="card"><p> Nombre: ${element.name}<p>
+     <p>Estado: ${element.status}<p>
+     <p> Specie: ${element.species}<p> <p>Genero: ${element.gender}<p><p>Lugar de Origen: ${element.origin.name}<p><p>Ubicacion Actual: ${element.location.name}<p>
+     <img src = "${element.image}"width="80%" height="90%"> </div>`
+    });
+     
+    card.innerHTML = str;
+    
+}
+
+
+buttonOtrosPersonajes.addEventListener('click', () => {
+    resultOtrosPersonajes(data)
+});
